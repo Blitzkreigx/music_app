e05c8b303eb423b434b3d393aae117a0d0a4017f
ChrlstlanX <chrlstl4nk09@gmail.com>
2024-04-03 14:21:18 -0600

    music app 1.0.0

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -12,29 +12,48 @@ const musicList = [
         'name': 'Hit The Road Jack',
         'author': 'Ray Charles',
         'image': './src/images/hit-the-road-jack.jpeg',
-        'audio': new Audio('./src/images/hit-the-road-jack.mp3')
+        'audio': new Audio('./src/audios/hit-the-road-jack.mp3')
     },
     {
         'name': 'Georgia On My Mind',
         'author': 'Ray Charles',
         'image': './src/images/georgia-on-my-mind.webp',
-        'audio': new Audio('./src/images/georgia-on-my-mind.mp3')
+        'audio': new Audio('./src/audios/georgia-on-my-mind.mp3')
     },
     {
-        'name': 'kerosene',
-        'author': 'Crystal Castles',
-        'image': './src/images/kerosene.jpeg',
-        'audio': new Audio('./src/images/kerosene.mp3')
+        'name': 'Fever',
+        'author': 'Peggy Lee',
+        'image': './src/images/fever.jpeg',
+        'audio': new Audio('./src/audios/fever.mp3')
+    },
+    {
+        'name': 'I Just Want To Make Love To You',
+        'author': 'Etta James',
+        'image': './src/images/i-just-want-to-make-love-to-you.jpeg',
+        'audio': new Audio('./src/audios/i-just-want-to-make-love-to-you.mp3')
+    },
+    {
+        'name': 'I Put a Spell On You',
+        'author': 'Nina Simone',
+        'image': './src/images/i-put-a-spell-on-you.jpeg',
+        'audio': new Audio('./src/audios/i-put-a-spell-on-you.mp3')
     },
 ];
 
-// let timeConvert;
+
+let numberList = 0;
+let indice = musicList[numberList];
 
 buttonPrevious.addEventListener('click', () => {
     console.log('Escuchando música anterior!');
+    lastValue = 0;
+    const music = indice;
+    music.audio.pause();
+    clearInterval(interval);
+    buttonStop.style.display='none';
+    buttonPlay.style.display='block';
 
     numberList--;
-
     if (numberList < 0) {
         numberList = musicList.length - 1;
     }
@@ -42,9 +61,6 @@ buttonPrevious.addEventListener('click', () => {
     indice = musicList[numberList];
     updateInfoSong();
 });
-
-let numberList = 0;
-let indice = musicList[numberList];
 
 function initializePage() {
     range.value = 0;
@@ -73,17 +89,21 @@ function initializeAudioMetadata(audioElement) {
 
         range.setAttribute('min', 0);
         range.setAttribute('max', timeConvert);
-        console.log(timeConvert);
     });
 };
 
 document.addEventListener('DOMContentLoaded', initializePage);
 
 buttonNext.addEventListener('click', () => {
-    //
-        range.value=(0)
-        actuallyRange.textContent=('00:00')
-    //
+    const music = indice
+    music.audio.pause()
+    clearInterval(interval)
+    buttonStop.style.display='none'
+    buttonPlay.style.display='block'
+
+    lastValue = 0
+    range.value=(0)
+    actuallyRange.textContent=('00:00')
     console.log('Escuchando música siguiente!');
     numberList++;
 
@@ -92,8 +112,6 @@ buttonNext.addEventListener('click', () => {
     }
 
     indice = musicList[numberList];
-    console.log(`Canción actual: ${indice.name} - ${indice.author}`);
-
     updateInfoSong();
 });
 
@@ -103,8 +121,8 @@ let interval;
 buttonPlay.addEventListener('click', () => {
     buttonStop.style.display='block'
     buttonPlay.style.display='none'
-    const musicone = indice
-    musicone.audio.play()
+    const music = indice
+    music.audio.play()
 
     function IncrementRange(increment, limite) {
         let initialValue = lastValue;
@@ -126,18 +144,16 @@ buttonPlay.addEventListener('click', () => {
     }
 
     IncrementRange(1, timeConvert)
-    range.value=(0)
 });
 
 buttonStop.addEventListener('click', () => {
-    buttonStop.style.display='none'
-    buttonPlay.style.display='block'
-    const musicone = indice
-    musicone.audio.pause()
+    buttonStop.style.display='none';
+    buttonPlay.style.display='block';
+    const music = indice;
+    music.audio.pause();
 
-    clearInterval(interval)
+    clearInterval(interval);
     lastValue = parseInt(range.value);
-    console.log('Música Detenida!')
 });
 
 function updateInfoSong() {
@@ -149,10 +165,10 @@ function updateInfoSong() {
     image.setAttribute('src', indice.image);
     image.setAttribute('alt', indice.name);
 
-    const musicone = indice.audio;
-    musicone.pause();
-    musicone.currentTime = 0;
-    initializeAudioMetadata(musicone);
+    const music = indice.audio;
+    music.pause();
+    music.currentTime = 0;
+    initializeAudioMetadata(music);
 };
 
 buttonAddToFavorite.addEventListener('click', () => {
